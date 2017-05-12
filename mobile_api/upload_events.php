59145910ab84a5f3687687f904b28f755c31a04c
presisco <internight@sina.com>
2017-05-12 20:50:02 +0800

    adjust log settings

--- mobile_api/upload_events.php
+++ mobile_api/upload_events.php
@@ -22,7 +22,7 @@ $sql="insert into user_health_event(username,event_type,body_sign,averate_stats,
 
 // add data to prototype
 foreach ($event_array as $event){
-	log_to_file("event array: ".$event);
+	log_to_file("event var type: ".gettype($event));
 	/*
 	$sql=$sql 
 		. "(" . $event->username
